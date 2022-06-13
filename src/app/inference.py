aaaf6c536fe08721ef7e13ba928875a94320c53a
wisoffe <wisoffe@gmail.com>
2022-06-13 12:31:38 +0300

    Change app to ent-to-end inference

--- src/app/inference.py
+++ src/app/inference.py
@@ -6,6 +6,10 @@ import pandas as pd
 import mlflow
 from dotenv import load_dotenv
 from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.responses import FileResponse
+from src.data.generate_pairs import generate_pairs_df
+from src.features.build_pair_features import build_pair_feautures_df
+from src.models.predict_model import predict_model_in_memory, PAIRS_DROP_ORDER_DUBLICATES
 
 # Load the environment variables from the .env file into the application
 load_dotenv(override=True)
@@ -19,7 +23,7 @@ app = FastAPI()
 class Model:  # pylint: disable=too-few-public-methods
     """General model class for inference"""
 
-    def __init__(self, model_name, model_stage):
+    def __init__(self, model_name: str, model_stage: str, inference_params: dict) -> None:
         """
         To initialize the model
         model_name: Name of the model in registry
@@ -27,34 +31,49 @@ class Model:  # pylint: disable=too-few-public-methods
         """
         # Load the model from Registry
         self.model = mlflow.pyfunc.load_model(f"models:/{model_name}/{model_stage}")
+        self.inference_params = inference_params
 
-    def predict(self, data):
+    def predict(self, df_dataset: pd.DataFrame) -> pd.DataFrame:
         """
         To use the loaded model to make predictions on the data
         data: Pandas DataFrame to perform predictions
         """
-        predictions = self.model.predict(data)
-        return predictions
 
+        df_pairs, _ = generate_pairs_df(
+            df_dataset,
+            drop_order_dub=self.inference_params["drop_order_dub"],
+            get_metrics=False,
+            real_test=True,
+        )
+
+        df_pair_feautures = build_pair_feautures_df(df_pairs, add_target_label=False)
+
+        submission_pred = predict_model_in_memory(self.model, df_dataset[["id"]], df_pair_feautures)
+
+        return submission_pred
+
+
+global_inference_params = {"drop_order_dub": PAIRS_DROP_ORDER_DUBLICATES}
 
 # Create model
-model = Model("general_model_xboost", "Staging")
+model = Model("general_model_xboost", "Staging", global_inference_params)
 
 
 # Create the POST endpoint with path '/invocations'
-@app.post("/invocations")
+@app.post("/invocations", response_class=FileResponse)
 async def create_upload_file(file: UploadFile = File(...)):
-    """Generate prediction by file .csv
+    """Generates .csv file of the submission format by input .csv file of the original format.
 
     Args:
-        file (UploadFile, optional): file .csv with columns of model signatures.
+        file (UploadFile, optional): file .csv with columns:
+            id,name,latitude,longitude,address,city,state,zip,country,url,phone,categories
             Defaults to File(...).
 
     Raises:
         HTTPException: If file not .csv
 
     Returns:
-        _type_: Json of model prediction
+        FileResponse: Return File Response (file in .csv format with columns: id,matches)
     """
     # Handle the file only if it is a CSV
     if file.filename.endswith(".csv"):  # pylint: disable=no-else-return
@@ -66,9 +85,10 @@ async def create_upload_file(file: UploadFile = File(...)):
         data = pd.read_csv(file.filename)
         os.remove(file.filename)  # Return a JSON object containing the model predictions
 
-        # без преобразования каждого элемента в int, получал ошибку
-        # TypeError: 'numpy.int32' object is not iterable
-        return list(map(int, model.predict(data)))
+        # response file path
+        response_file_path = "submission.csv"
+        model.predict(data).to_csv(response_file_path, index=False)
+        return response_file_path
 
     else:
         # Raise a HTTP 400 Exception, indicating Bad Request
